c6937fe63ad9be9776b7b188c93f53bb0d75af84
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-21 06:52:59 +0300

    Initial commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const LoginPage());
+    return const MaterialApp(home: LoginPage());
   }
 }
