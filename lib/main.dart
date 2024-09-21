4fde8342e73413292bf4b83abab6db350df02f4a
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-21 06:46:46 +0300

    Initial commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:auth_flutter/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,14 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: 
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: LoginPage());
   }
 }
-
