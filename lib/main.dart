e3547cd7d245aa21b6202796f65ded6c389982f8
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-21 06:54:55 +0300

    Initial commit for lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginPage());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
