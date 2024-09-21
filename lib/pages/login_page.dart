84c3dfbc27fcb9317266a18a2d73b1a98368a545
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-21 07:04:11 +0300

    initial commit for loginPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+    );
   }
 }
