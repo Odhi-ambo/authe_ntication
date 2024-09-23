db10a9eef237f3f0b22c2390385f0e5402c03e34
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-23 08:43:10 +0300

    my textfield components

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -4,7 +4,11 @@ class MyTextfield extends StatelessWidget {
   final controller;
   final String hintText;
   final bool obscureText;
-  const MyTextfield({super.key, required this.controller, required this.hintText, required this.obscureText});
+  const MyTextfield(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText});
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +16,7 @@ class MyTextfield extends StatelessWidget {
         Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
             //borderRadius: BorderRadius.all(),
