cf64f3f1ddba9ea101798179c9216b2f0ff039f3
Odhi-ambo <michaelderrick605@gmail.com>
2024-09-26 09:18:03 +0300

    commit for logo square tile

--- lib/components/square_tile.dart
+++ lib/components/square_tile.dart
@@ -7,7 +7,7 @@ class SquareTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
           border: Border.all(color: Colors.white),
           borderRadius: BorderRadius.circular(10),
